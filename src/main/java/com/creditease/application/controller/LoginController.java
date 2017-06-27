e2cc1aa3d46a729db13300c5574190e149628264
huiyangchen <hnrchy@163.com>
2017-06-27 17:13:27 +0800

    提交

---
+++ src/main/java/com/creditease/application/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.creditease.application.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.creditease.application.entity.User;
+import com.creditease.application.request.Pager;
+import com.creditease.application.request.ResultInfo;
+import com.creditease.application.request.UserBean;
+import com.creditease.application.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Created by huiyangchen1 on 2017/6/16.
+ */
+@Controller
+@RequestMapping(value = "/login")
+public class LoginController {
+
+    @Autowired
+    private UserService userService;
+
+
+    @RequestMapping("/")
+    public String toLogin(HttpServletRequest request){
+        return "index";
+    }
+
+}
---
+++ src/main/resources/static/js/index.js
@@ -0,0 +1,26 @@
+    function addTab(title, url){
+    	if ($('#tt').tabs('exists', title)){
+    		$('#tt').tabs('select', title);
+    	} else {
+    		var content = '<iframe scrolling="auto" frameborder="0"  src="'+baseUrl+url+'" style="width:100%;height:100%;"></iframe>';
+    		$('#tt').tabs('add',{
+    			title:title,
+    			content:content,
+    			closable:true
+    		});
+    	}
+    }
+
+    $(document).ready(function () {
+               $('.easyui-accordion li a').click(function () {
+                   var tabTitle = $(this).text();
+                   var url = $(this).attr("href");
+                   addTab(tabTitle, url);
+                   $('.easyui-accordion li div').removeClass("selected");
+                   $(this).parent().addClass("selected");
+               }).hover(function () {
+                   $(this).parent().addClass("hover");
+               }, function () {
+                   $(this).parent().removeClass("hover");
+               });
+      })
